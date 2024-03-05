564b07d5db181734e0d13585df0e88eefb86cfd3
Nicolas44Hernandez <n44hernandezp@gmail.com>
2024-03-05 17:26:11 +0100

    WIP: write to file

--- main.c
+++ main.c
@@ -17,7 +17,11 @@
 #define DM_BACKEND_USP    "/usr/bin/mods/amxb/mod-amxb-usp.so"
 
 // key of datamodel to search
-#define DM_DEVICE_KEY  "DeviceInfo."
+//#define DM_DEVICE_KEY  "DeviceInfo."
+#define DM_DEVICE_KEY  "WiFi.Radio.1.Stats."
+
+// key of datamodel to WiFi
+#define DM_WIFI_KEY "WiFi."
 
 // global bus variable
 static amxb_bus_ctx_t *bus_ctx_usp = NULL;
@@ -49,8 +53,7 @@ int config_api_init() {
 }
 
 
-int get_dm_friendly_name()
-{
+int get_dm_friendly_name() {
   amxc_var_t results;
 
   // init variable
@@ -64,10 +67,14 @@ int get_dm_friendly_name()
       return -1;
   }
   // dump all datamodel info under the key for debug
-  // amxc_var_dump(&results, STDOUT_FILENO);
+  amxc_var_dump(&results, STDOUT_FILENO);
 
   // helper to iterate on result object
   amxc_var_for_each(var, &results) {
+    //print var
+    printf("var:");
+    printf("\n");
+    printf(var);
     // get the datamodel
     amxc_var_t* objectvar = GET_ARG(var, DM_DEVICE_KEY);
     // get the friendlyname value
@@ -81,14 +88,96 @@ int get_dm_friendly_name()
   return 0;
 }
 
+int get_wifi_stats() {
+  amxc_var_t results;
+
+  // init variable
+  amxc_var_init(&results);
+
+  // get datamodel key from usp endpoint
+  int rv = amxb_get(bus_ctx_usp, DM_WIFI_KEY, 0, &results, 1);
+  if(rv != 0) {
+      printf("CONFIG DM: failed to get config");
+      amxc_var_clean(&results);
+      return -1;
+  }
+  // dump all datamodel info under the key for debug
+  amxc_var_dump(&results, STDOUT_FILENO);
+
+  // helper to iterate on result object
+  // amxc_var_for_each(var, &results) {
+  //   // get the datamodel
+  //   amxc_var_t* objectvar = GET_ARG(var, DM_WIFI_KEY);
+  //   // get the friendlyname value
+  //   const char* val = GET_CHAR(objectvar, "MulticastPacketsSent");
+  //   // print the value
+  //   printf("MulticastPacketsSent:");
+  //   printf(val);
+  //   printf("\n");
+  // }
+  // release the object result
+  amxc_var_clean(&results);
+  return 0;
+}
+
+int get_wifi_radio_stats(){
+  amxc_var_t values;
+  amxc_var_init(&values);  
+
+  //int rv = amxb_get(bus_ctx_usp, "WiFi.Radio.*.Stats.", 0, &values, 1);
+  int rv = amxb_get(bus_ctx_usp, "WiFi.Radio.*.getRadioStats()", 0, &values, 1);
+  amxc_var_dump(&values, STDOUT_FILENO); 
+
+  amxc_var_clean(&values);
+}
+
+
+int write_to_file(){
+
+
+  /*Define file name*/
+  char file_name[] = "/data/test.txt";
+  /*Define string to append*/
+  char string_to_append[] = "Nico";
+
+
+  /*Opening the file in appending mode...*/
+  fp = fopen(file_name, "a+");
+  /*File open operation failed.*/
+  if (fp == NULL) {
+    printf("RESULTS FILE [ERROR]: failed to open results file\n");
+    return -1;
+  }
+
+  /*Appending string to the file...*/
+  printf("Appending string...\n");
+  fprintf(fp, "Your string: %s\n", string_to_append);
+  
+  /*Closing the file...*/
+  fclose(fp);
+  
+  printf("Successfully appended\n");
+  return 0;
+
+}
+
 
 int main() {
-  printf("Smart Local Networks application start!\n");
+  //printf("Smart Local Networks application start!\n");
 
   // configure amx connection
   config_api_init();
   // get the FriendlyName from host
-  get_dm_friendly_name();
+  //get_dm_friendly_name();
+
+  // get WiFi stats
+  //get_wifi_stats();
+
+  // get wifi radio stats
+  //get_wifi_radio_stats();
+
+  // write to file
+  write_to_file();
 
   // clean amxrt configuration
   amxrt_delete();
